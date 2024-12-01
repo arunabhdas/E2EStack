effa59c363c9c4cd8a30485360bfd5199af4a904
Arunabh Das <arunabhdas@gmail.com>
2024-12-01 08:15:09 -0500

    Added venv to .gitignore

--- E2EStack/main.py
+++ E2EStack/main.py
@@ -2,12 +2,16 @@ import os
 
 from dotenv import load_dotenv
 
+from langchain.llms import OpenAI
+
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 def main():
-    print(OPENAI_API_KEY)
+    llm = OpenAI(temperature=0.9, openai_api_key=OPENAI_API_KEY)
+    result = llm.predict("Give me 5 topics for interesting YouTube videos about Python")
+    print(result)
 
 if __name__ == "__main__":
     main()
