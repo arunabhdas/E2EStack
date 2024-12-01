3b00064488b472548cd35be1e7966e0807ebbb61
Arunabh Das <arunabhdas@gmail.com>
2024-12-01 08:41:39 -0500

    Switching to gpt-4

--- E2EStack/main.py
+++ E2EStack/main.py
@@ -2,15 +2,19 @@ import os
 
 from dotenv import load_dotenv
 
-from langchain.llms import OpenAI
+from langchain_community.llms import OpenAI
+from langchain_community.chat_models import ChatOpenAI
 
 load_dotenv()
 
+OPENAI_MODEL = "gpt-4"
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
 def main():
-    llm = OpenAI(temperature=0.9, openai_api_key=OPENAI_API_KEY)
-    result = llm.predict("Give me 5 topics for interesting YouTube videos about Python")
+
+    llm = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model=OPENAI_MODEL)
+    # llm = OpenAI(temperature=0.9, openai_api_key=OPENAI_API_KEY)
+    result = llm.invoke("Give me 5 topics for interesting YouTube videos about Python")
     print(result)
 
 if __name__ == "__main__":
