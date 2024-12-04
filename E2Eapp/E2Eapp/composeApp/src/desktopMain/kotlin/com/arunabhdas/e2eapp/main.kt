7396c37b64ed8d7b3e51aa596fb9e238885e8be6
Arunabh Das <arunabhdas@gmail.com>
2024-12-04 03:53:25 -0500

    Added Settings window

--- E2Eapp/E2Eapp/composeApp/src/desktopMain/kotlin/com/arunabhdas/e2eapp/main.kt
+++ E2Eapp/E2Eapp/composeApp/src/desktopMain/kotlin/com/arunabhdas/e2eapp/main.kt
@@ -1,13 +1,41 @@
 package com.arunabhdas.e2eapp
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 
 fun main() = application {
+    var secondWindowOpened by remember { mutableStateOf(true) }
     Window(
         onCloseRequest = ::exitApplication,
         title = "E2Eapp",
     ) {
         App()
+        if (secondWindowOpened) {
+            Window(
+                onCloseRequest = { secondWindowOpened = false },
+                title = "Settings",
+            ) {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {}) {
+                        Text("Settings")
+                    }
+                }
+            }
+        }
     }
 }
